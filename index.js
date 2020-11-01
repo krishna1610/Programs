c41090bcf96364f67ae197e38234b6b21ed4e86f
Krishna <krishna@Krishnas-MBP.lan>
2020-11-01 12:38:52 -0800

    function

---
+++ index.js
@@ -0,0 +1,4 @@
+/* 1) Write a function isMultiple that accepts two non-negative number parameters a and b, 
+and returns true if a is a multiple of b, and false otherwise. For example, the call of 
+isMultiple(15, 5) would return true because 15 = 5 * 3. You may assume that a and b are 
+non-negative integers and that b is not 0. */
